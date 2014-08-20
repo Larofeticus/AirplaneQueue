8d1878efd22983ecb3efc706000b95932f6639ce
Bill Arndt <barndt@cobroventures.com>
2014-08-20 00:30:44 -0400

    adding waiting attribute to class which will track number of planes waiting

--- AirplaneQueue.rb
+++ AirplaneQueue.rb
@@ -1,3 +1,7 @@
 class AirplaneQueue
+  attr_accessor :waiting
 
+  def initialize
+    @waiting = 0
+  end
 end
--- spec/AirplaneQueue_spec.rb
+++ spec/AirplaneQueue_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe AirplaneQueue do
-
+  describe "initialize" do
+    it "creates an empty airplane queue" do
+      aq = AirplaneQueue.new
+      expect(aq.waiting).to eq 0
+    end
+  end
 end
