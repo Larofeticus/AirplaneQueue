c9b4c64f0e8f80e4381c537d04206eca56b1c4e8
Bill Arndt <barndt@cobroventures.com>
2014-08-20 10:02:36 -0400

    _buildType method and tests

--- AirplaneQueue.rb
+++ AirplaneQueue.rb
@@ -1,7 +1,43 @@
+#AirplaneQueue: track several types of airplanes that wish to pass through a bottleneck. 
+#Airplanes have two characteristics: A size that can be small or large
+#                                    A type that can be passenger or cargo
+#The methods are:
+#  enqueue: accepts (String, Hash) hash determines plane type using keys :size and :type, 
+#              values as strings: small, large:, passenger, cargo
+#           returns true when successful, false if the plane was not enqueued for some reason
+#  dequeue: removes highest priority plane from queue, returns it. returns nil if queue is empty
 class AirplaneQueue
-  attr_accessor :waiting
+  #current number of planes in queue, not in requirments but used for testing
+  attr_reader :waiting
+
+  #this constant is used to name each accepted aircraft type and determines their 
+  #relative priority, 0 is highest
+  ACTYPES = ["largepassenger","smallpassenger","largecargo","smallcargo"]
+
+  #the state is stored in @queue, which is a Hash of ACTYPES to arrays, of which each array
+  #holds all plane name strings  of the corresponding type in the order of their arrival
 
   def initialize
     @waiting = 0
+
+    @queue = { }
+ 
+    #initialize a queue for each type of ac
+    ACTYPES.each { |type| @queue[type] = [] } 
+  end
+
+  def dequeue
+    return nil if @waiting == 0
+  end
+
+  #helper method, takes type hash from a new airplane, returns it's type string
+  #or nil if it doesn't have the :size and :type keys
+  def _buildType parms
+    return nil unless parms.class == Hash
+    return nil unless parms[:size]
+    return nil unless parms[:type]
+    s = "#{parms[:size]}#{parms[:type]}"
+    return nil unless @queue[s]
+    s
   end
 end
--- spec/AirplaneQueue_spec.rb
+++ spec/AirplaneQueue_spec.rb
@@ -1,10 +1,44 @@
 require 'spec_helper'
 
 describe AirplaneQueue do
+  
+  before :each do
+    @aq = AirplaneQueue.new
+  end
+
   describe "initialize" do
     it "creates an empty airplane queue" do
-      aq = AirplaneQueue.new
-      expect(aq.waiting).to eq 0
+      expect(@aq.waiting).to eq 0
+    end
+  end
+  
+  describe "enqueue" do
+
+  end
+  
+  describe "dequeue" do
+    it "returns nil when all queues are empty" do
+      expect(@aq.dequeue).to eq nil
+    end
+  end
+
+  describe "_buildType" do
+    it "returns nil when not given a hash" do
+      expect(@aq._buildType(1)).to eq nil
+    end
+    it "returns nil when not given :size key" do
+      expect(@aq._buildType(type: "cargo")).to eq nil
+    end
+    it "returns nil when not given :type key" do
+      expect(@aq._buildType(size: "small")).to eq nil
+    end
+    it "returns nil when assembed type name isn't an ACTYPE" do
+      expect(@aq._buildType(type: "cargenger", size: "small")).to eq nil
+    end
+    [{type: "cargo", size: "small", name: "smallcargo"}, {type: "cargo", size: "large", name: "largecargo"}, {type: "passenger", size: "small", name: "smallpassenger"}, {type: "passenger", size: "large", name: "largepassenger"}].each do |p|
+      it "returns #{p[:name]} when given type #{p[:type]} and size #{p[:size]}" do
+        expect(@aq._buildType(p)).to eq p[:name]
+      end
     end
   end
 end
